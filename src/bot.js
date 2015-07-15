695d0203e2fc2980ef6aeef095b41f27692c8e1e
Damon Oehlman <damon.oehlman@gmail.com>
2015-07-15 23:01:03 +1000

    most is awesome

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,17 @@
-alert('hello');
+var most = require('most');
+var qsa = require('fdom/qsa');
+
+most
+  .fromEvent('click', document)
+  .filter(function(evt) {
+    return evt.target &&
+      evt.target.matches('.SepButton') &&
+      evt.target.textContent === 'Build';
+  })
+  .delay(100)
+  .flatMap(function() {
+    return most.from(qsa('#BuildingScreen input[type="text"]'));
+  })
+  .observe(function(input) {
+    console.log('found input: ', input);
+  });
